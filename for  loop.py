1f0353780dd159c7328b871bb29d70580e2da171
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-07-26 14:07:52 +0530

    Add for loop examples for reverse and forward counting

---
+++ for  loop.py
@@ -0,0 +1,7 @@
+#print for lopp in reverse order
+print("the reverse for loop value is:");
+for i in range (20,0,-1):
+    print(i);
+#print value from 0 to number using for loop
+for i in range(1,101,1):
+  print(i);
