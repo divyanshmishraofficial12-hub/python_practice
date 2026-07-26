9962e29ce5d6613518c1ac98dbd1a161eb641ac6
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-07-26 14:39:26 +0530

    Update multiplication table to use user input
    Refactor multiplication table to take user input for number and print the table up to 10 times the input.

--- first.py
+++
@@ -1,6 +0,0 @@
-#range & for loop
-print("the table of number two is;")
-for i in range (1,10):
-   
-    print("2 x",i,"=",2*i);
- 
---
+++ table.py
@@ -0,0 +1,8 @@
+#range & for loop
+ n=int(input("enter the number..?"));
+z=1
+for i in range (n,n*10+1,n):
+  
+  
+    print(n,"x",z,"=", i);
+    z+=1
