90227acc4b96cf922f7d19e223698a68a760446d
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-07-26 14:11:00 +0530

    Implement negative numbering loop in for loop.py
    Added a loop to print negative numbers from -1 to -101.

--- for  loop.py
+++ for  loop.py
@@ -5,3 +5,8 @@ for i in range (20,0,-1):
 #print value from 0 to number using for loop
 for i in range(1,101,1):
   print(i);
+
+#the negative numbering are
+    
+for i in range (-1,-101,-1):
+    print ("the negative numbering from -1 to -101 are:",i);
