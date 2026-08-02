8be25b21b7da4d18a48f320857d2617c8b229764
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-02 13:22:24 +0530

    Implement summation of numbers up to n terms
    Added a loop to sum numbers up to n terms.

--- for  loop.py
+++ for  loop.py
@@ -51,3 +51,12 @@ n= int(input("enter the no of terms..?"))
 
 for i in range(n,1,-1):
     print(i)
+
+#Sum up to n terms
+n= int(input("enter the no of terms..?"))
+sum= 0
+for i in range(1,n+1):
+    sum+= i
+print(sum)
+    
+    
