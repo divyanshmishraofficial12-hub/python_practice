fe3c117867d8a30d1cb80d6f45a784122b599f2d
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-02 13:36:47 +0530

    Implement sum calculation for even and odd numbers
    Add functionality to print the sum of even and odd numbers separately based on user input.

--- for  loop.py
+++ for  loop.py
@@ -59,4 +59,17 @@ for i in range(1,n+1):
     sum+= i
 print(sum)
     
-    
+# - Print the sum of all even & odd numbers in a range
+#separately
+n= int(input("enter the no of terms..?"))
+sum= 0
+if n % 2 == 0:
+    for i in range(2,n+1,2):
+     sum+= i
+    print(sum)
+
+if n % 2!= 0:
+    for i in range(1,n+1,2):
+        sum+=i
+    print(sum)
+        
