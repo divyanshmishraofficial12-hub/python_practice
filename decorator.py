d8f1c7f4a640e88efbfb26cc70a473940ac9cd44
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-26 15:26:06 +0530

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -34,3 +34,28 @@ def game():
     print("thiss is my game..!")
 
 game()
+
+
+def mk(x):
+
+    def mk1():
+
+
+        print("Decorated")
+
+
+        x()
+
+
+    return mk1
+
+
+def mk2():
+
+    print("Ordinary")
+
+
+p = mk(mk2)
+
+
+p()
