a0cb88580c57db689990d1f705c5cd02c14e5da8
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-26 15:35:37 +0530

    Implement power function using lambda expressions
    Added a power function that returns a lambda for squaring and cubing a number.

--- lambda.py
+++ lambda.py
@@ -10,3 +10,12 @@ a= lambda x,y,z: x + y + z
 print(a(5,10,15))
 
 
+#this is a lambda function or ananomyous function
+
+def power(n):
+    return lambda a:a**n
+
+square=power(2)  #print square :it holds a:a ** 2
+cube=power(3)    #print cube   :it hols a:a ** 3
+print("the square is :",square(3))
+print("the cube is :",cube(3))
