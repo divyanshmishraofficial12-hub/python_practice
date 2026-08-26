f7a96916f20319de7e84a410e185672f137c40fc
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-26 15:24:08 +0530

    Add decorator for function logging
    Implement a decorator to wrap functions for logging before and after execution.

---
+++ decorator.py
@@ -0,0 +1,23 @@
+#decorators :- wrap one function in another function
+
+'''Isko aise samjho ki agar aapko apne 10 alag-alag functions ke aage ya peeche ek hi jaisa kaam (jaise logging karna, user ka permission check karna, ya timing measure karna) jodna ho, toh aapko bar-bar wahi code har function ke andar likhne ki zaroorat nahi padti.'''
+
+def decorator(func):
+    def wrapper():
+        print("it print BEFORE function..!")
+        func()
+        print("it print AFTER function..!")
+    return wrapper
+
+def hello():
+    print("this is my first function")
+
+deco=decorator(hello)
+deco() #s tarike mein humein function define karne ke baad ek alag se variable mein usko decorator ke andar pass karna padta hai:
+
+
+@decorator #  Yeh @ symbol Python mein shortcut (syntactic sugar) ki tarah kaam karta hai. Jaise aapke screenshot mein dikh raha hai:
+def sec():
+    print("this is a second function..!")
+
+sec()
