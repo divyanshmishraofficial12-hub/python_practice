ff0b047ffe5b14a81ec7326226305f29fa51547e
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-26 15:38:38 +0530

    Create module.py

---
+++ module.py
@@ -0,0 +1,13 @@
+# what is structured programming:-
+
+'''Jab hum code likhte hain, toh sab kuch ek hi lambi file mein likhne ke bajaye, use chhote, alag-alag hisso mein baant dena hi Structured Programming aur Modules ka concept hai'''
+
+#what is module:-
+
+'''Python ki ek aisi file (.py) jiske andar kuch specific functions ya code likha hota hai, aur use hum kisi doosre program mein import karke use kar sakte hain.'''
+
+# this used to organize code much better
+
+import func as f    # it is used to import another file or module
+
+f.hlo(5)  # using func from another file
