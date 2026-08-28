afabf4171752f2dc80e205d35e0710fc8b11317e
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-28 07:01:51 +0530

    Add range examples to list.py
    Added examples of creating lists using the range function.

--- list.py
+++ list.py
@@ -3,3 +3,15 @@
 a=[20,33,44,44,4]
 print(a)
 print(type(a))
+
+
+#range in list
+
+l1=list(range(11))
+print("the list that pribnts:-",l1)
+
+l2=list(range(5,25))
+print(l2)
+
+l3=list(range(1,22,2))
+print(l3)
