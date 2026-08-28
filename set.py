f8e8fa73d8a07a566e6fd473097697cd1210023b
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-28 10:43:37 +0530

    Implement set operations: union, intersection, symmetric difference
    Added union, intersection, and symmetric difference operations for sets.

--- set.py
+++ set.py
@@ -17,3 +17,12 @@ print(s1)
 print(s1)
 (s1.remove(2))  #it shows error if the value is not present in a set
 print(s1)
+
+s2={1,2,3,7,8,9}
+
+s3=s1.union(s2) # print all items adding both
+print(s3)
+s4=s1.intersection(s2)  #
+print(s4) #print only unique values
+s5=s1.symmetric_difference(s2)
+print(s5)
