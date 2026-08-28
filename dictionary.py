5fb0c6e3e441d898c46f25c62d82d64b1236ce85
Divyansh Mishra <divyanshmishraofficial12@gmail.com>
2026-08-28 14:02:04 +0530

    Add dictionary example with key-value pairs

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+# dictionary is ordered , changable , allow duplicates but not keys consists of key:value pairs
+
+d1={
+    'name':'divyansh',
+    'age':19,
+    'car': ['ford','wagon']
+}
+print(d1)
+print(len(d1))
+print(type(d1))
